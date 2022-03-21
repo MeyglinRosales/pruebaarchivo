0b1abfb9078fd14d3e528c113c38c60f2f6d8611
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-03-21 11:48:01 -0600

    Pruebs

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../persona.h"
+using namespace std;
+
+int main()
+{
+    persona ejemplopersona;
+
+    return 0;
+}
---
+++ src/persona.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "../../persona.h"
+
+persona::persona()
+{
+    cout << "Hello world!" << endl;
+    //ctor
+}
+
+persona::~persona()
+{
+    //dtor
+}
